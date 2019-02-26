eda7c13e4d9a499dea8f4590a9de3838956df948
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-02-26 11:33:19 +0100

    wip

--- src/screens/RockPaperScissors.jsx
+++ src/screens/RockPaperScissors.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 // import { Redirect } from 'react-router-dom';
 import Section from '../components/Section';
 
+// 1- change any code in Container
+// so that this compoonent receives history, match and location
 
+// 2- on component mount, check if we have a 'authed' key in localstorage
+// with a value of 'true'. If so do nothing else, otherwise use
+// history.push() to redirect to the home page
 export default class RockPaperScissors extends React.Component {
   render() {
     // console.log(this.props);
